292704f5521f301b17a98d336939798f320dba0c
Mingze Sun <122427329+Mingze2379@users.noreply.github.com>
2024-04-18 00:25:16 -0600

    init

---
+++ Main.py
@@ -0,0 +1,71 @@
+from Diffusion.Train import train
+import os
+from typing import Dict
+
+import torch
+import torch.optim as optim
+from torchvision.utils import save_image
+
+from Diffusion import GaussianDiffusionSampler, GaussianDiffusionTrainer
+from Diffusion.Model import UNet
+
+def eval(modelConfig: Dict):
+    # load model and evaluate
+    with torch.no_grad():
+        device = torch.device(modelConfig["device"])
+        model = UNet(T=modelConfig["T"], ch=modelConfig["channel"], ch_mult=modelConfig["channel_mult"], attn=modelConfig["attn"],
+                     num_res_blocks=modelConfig["num_res_blocks"], dropout=0.)
+        ckpt = torch.load(os.path.join(
+            modelConfig["save_weight_dir"], modelConfig["test_load_weight"]), map_location=device)
+        model.load_state_dict(ckpt)
+        print("model load weight done.")
+        model.eval()
+        sampler = GaussianDiffusionSampler(
+            model, modelConfig["beta_1"], modelConfig["beta_T"], modelConfig["T"]).to(device)
+        # Sampled from standard normal distribution
+        noisyImage = torch.randn(
+            size=[modelConfig["batch_size"], 3, 32, 32], device=device)
+        saveNoisy = torch.clamp(noisyImage * 0.5 + 0.5, 0, 1)
+        save_image(saveNoisy, os.path.join(
+            modelConfig["sampled_dir"], modelConfig["sampledNoisyImgName"]), nrow=modelConfig["nrow"])
+        sampledImgs = sampler(noisyImage)
+        sampledImgs = sampledImgs * 0.5 + 0.5  # [0 ~ 1]
+        save_image(sampledImgs, os.path.join(
+            modelConfig["sampled_dir"],  modelConfig["sampledImgName"]), nrow=modelConfig["nrow"])
+
+def main(model_config = None):
+    modelConfig = {
+        "state": "train", # or eval
+        "epoch": 200,
+        "batch_size": 80,
+        "T": 1000,
+        "channel": 128,
+        "channel_mult": [1, 2, 3, 4],
+        "attn": [2],
+        "num_res_blocks": 2,
+        "dropout": 0.15,
+        "lr": 1e-4,
+        "multiplier": 2.,
+        "beta_1": 1e-4,
+        "beta_T": 0.02,
+        "img_size": 32,
+        "grad_clip": 1.,
+        "device": "cuda:0", ### MAKE SURE YOU HAVE A GPU !!!
+        "training_load_weight": None,
+        "save_weight_dir": "./Checkpoints/",
+        "test_load_weight": "ckpt_199_.pt",
+        "sampled_dir": "./SampledImgs/",
+        "sampledNoisyImgName": "NoisyNoGuidenceImgs.png",
+        "sampledImgName": "SampledNoGuidenceImgs.png",
+        "nrow": 8
+        }
+    if model_config is not None:
+        modelConfig = model_config
+    if modelConfig["state"] == "train":
+        train(modelConfig)
+    else:
+        eval(modelConfig)
+
+
+if __name__ == '__main__':
+    main()
